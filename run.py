a72aaa41154cb98119add68a8976d4ef08c01528
sree0831 <sreelakshmi.passavula@gmail.com>
2020-06-25 19:13:53 +0000

    routing reservation from ,added flash

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
   
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-
+app.secret_key = 'some_secret'
 
 @app.route('/')
 def index():
@@ -15,9 +15,13 @@ def menu():
     return render_template("menu.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        flash("Thanks {}, we have received your reservation!".format(
+            request.form["name"]
+        ))
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route('/recipes')
