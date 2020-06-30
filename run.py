f1e7b6f52fad0081606882b8d766a05ab3c67d3a
sree0831 <sreelakshmi.passavula@gmail.com>
2020-06-30 10:04:04 +0000

    changed os path in run.py

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask, render_template, request, flash ,redirect ,url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
